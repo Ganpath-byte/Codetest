809cd3bb369aed5276e39816dcbfbd58119cc040
Ganpath-byte <gr379@njit.edu>
2024-11-12 19:11:01 -0500

    commit

---
+++ codetest8/codetest8.js
@@ -0,0 +1,70 @@
+var age = 20;
+
+var age;
+
+age = 100;
+
+console.log(age);
+
+let rollNo = 21;
+
+rollNo = 101;
+
+console.log(rollNo);
+
+const valuePi = 3.14;
+
+valuePi = 200;
+
+console.log(valuePi);
+
+let pet = true
+
+console.log(pet)
+
+let house = null;
+
+console.log(house)
+
+house = "slytherin"
+
+let bigNumber = 12345678909876543234567890n;
+
+console.log(bigNumber);
+
+let firstname = "Harry"
+
+let lastname = "Potter"
+
+console.log(firstname, lastname);
+
+let a = 100;
+
+let b = 20;
+
+console.log(a % b);
+
+// a += 10; a = a + 10;
+
+a = a + 10;
+
+a *= 10; a = a * 10
+
+console.log(a);
+
+console.log(a == b);
+
+let c = true;
+
+let d = false;
+
+console.log(c && d);
+
+// let house;
+
+// let firstname = "Harry";
+
+if (firstname == "Harry"){
+    house = "Gryffindor";
+    console.log(house);
+}
